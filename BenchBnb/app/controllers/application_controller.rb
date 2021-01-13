9fc71a212ec2b50911e4d56bfd893429f9ef1ac3
asiddiki98 <azimsardines@gmail.com>
2021-01-13 17:01:47 -0500

    reducers

--- BenchBnb/app/controllers/application_controller.rb
+++ BenchBnb/app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+    # protect_from_forgery :except => [:create, :destroy] 
+
+    helper_method :current_user
 
     def current_user
        @current_user ||= User.find_by(session_token: session[:session_token])
--- BenchBnb/frontend/bench_bnb.jsx
+++ BenchBnb/frontend/bench_bnb.jsx
@@ -1,8 +1,23 @@
-
+import { signUp, login, logout } from './util/session_api_util'
+import createStore from './store/store'
 import React from "react";
 import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
+    const user = { user: {username: "gigi", password: "hunter12"}}
+    window.user = user
+    window.signUp = signUp
+    window.login = login
+    window.logout = logout
+
+    if (window.currentUser){
+        preloadedState = {
+            session: {
+                currentUser: window.currentUser
+            }
+        }
+    }
+    const store = createStore(preloadedState)
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
